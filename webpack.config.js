8d8137c1ee9212b5ceefa1c18a1e3b30a88d277b
Sam Nnodim <sam.nnodim@gmail.com>
2016-06-16 00:51:43 -0400

    updated webpack.config.js w/ path variables and a better webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const webpack = require('webpack');
 const fs      = require('fs');
 const path    = require('path');
-const join    = path.join,
-const resolve = path.resolve;
-
+      join    = path.join,
+      resolve = path.resolve;
 const getConfig = require('hjs-webpack');
+
+// Path Variables
+const root     = resolve(__dirname);
+const src      = resolve(root, 'src');
+const modules  = resolve(root, 'node_modules');
+const dist     = resolve(root, 'dist');
+
+var config = getConfig(
+  in: join(__dirname, 'src/app.js'),  // app entry point
+  out: join(__dirname, 'dist'),       // output folder to build to
+  clearBeforeBuild: true
+);
