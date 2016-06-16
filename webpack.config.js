323ceaeb17896e379d3a654a6d59c8c252eb8cc6
Sam Nnodim <sam.nnodim@gmail.com>
2016-06-16 00:24:57 -0400

    updated webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack');
+const fs      = require('fs');
+const path    = require('path');
+const join    = path.join,
+const resolve = path.resolve;
+
+const getConfig = require('hjs-webpack');
