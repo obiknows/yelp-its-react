0ed3862fe555e498492b13469183b7071ea39101
Sam Nnodim <sam.nnodim@gmail.com>
2016-06-16 01:22:31 -0400

    Finished webpack. Completely set up. Not tested however

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,17 @@ const src      = resolve(root, 'src');
 const modules  = resolve(root, 'node_modules');
 const dist     = resolve(root, 'dist');
 
+// Check if 'Dev' or 'Production'
+const NODE_ENV = process.env.NODE_ENV;
+const isDev    = NODE_ENV === 'development';
+
+
 var config = getConfig(
+  isDev: isDev,
   in: join(__dirname, 'src/app.js'),  // app entry point
   out: join(__dirname, 'dist'),       // output folder to build to
   clearBeforeBuild: true
 );
+
+// Export the config object
+module.exports = config;
