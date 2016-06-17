345ccc18b99d4b8cd5f3be05d639053c2d9920fe
Sam Nnodim <sam.nnodim@gmail.com>
2016-06-16 23:51:55 -0400

    Fucked around w/ app.css

--- webpack.config.js
+++ webpack.config.js
@@ -33,18 +33,36 @@ config.postcss = [].concat([
   require('cssnano')({})
 ])
 
-// // CSS Naming config
-// const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
-// const matchCssLoaders = /(^|!)(css-loader)($|!)/;
-//
-// const findLoader = (loaders, match) => {
-//   const found = loaders.filter(l => l &&
-//     l.loader && l.loader.match(match));
-//   return found ? found[0] : null;
-// }
-//
-// const cssloader = findLoader(config.module.loaders, matchCssLoaders);
-//
+// CSS Naming config
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l &&
+    l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+}
+
+const cssloader = findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader
+    .replace(matchCssLoaders,
+    `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+config.module.loaders.push(newloader);
+cssloader.test =
+  new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
 
 // Export the config object
 module.exports = config;
